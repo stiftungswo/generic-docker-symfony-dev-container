27fd76c741d4e5d992d2164a76d8fd000beebf11
toasterson <toasterson@gmail.com>
2014-09-05 14:11:24 +0200

    Added: ps File for viewing all Containers
    Modified: build.sh should now be executable from outside of subdir.

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-sudo docker build --rm -t swodev/base base
-sudo docker build --rm -t swodev/apache apache
+SCRIPT=$(realpath $0)
+SCRIPTPATH=$(dirname $SCRIPT)
+APP_PATH=$(realpath ${SCRIPTPATH}/../)
 
-sudo docker build --rm -t swodev/apache-php:prod apache-php/prod
-sudo docker build --rm -t swodev/apache-php:dev  apache-php/dev
+sudo docker build --rm -t swodev/base $SCRIPTPATH/base
+sudo docker build --rm -t swodev/apache $SCRIPTPATH/apache
 
-sudo docker build --rm -t swodev/app  app
+sudo docker build --rm -t swodev/apache-php:prod $SCRIPTPATH/apache-php/prod
+sudo docker build --rm -t swodev/apache-php:dev  $SCRIPTPATH/apache-php/dev
+
+sudo docker build --rm -t swodev/app  $SCRIPTPATH/app
